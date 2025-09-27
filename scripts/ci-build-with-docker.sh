1815073d14921d2b7ab4722579e1ce8d5e22ab0b
Anthony Astolfi <tastolfi@gmail.com>
2025-09-27 15:03:01 -0400

    wip

--- scripts/ci-build-with-docker.sh
+++ scripts/ci-build-with-docker.sh
@@ -14,17 +14,14 @@ PROJECT_DIR=$(realpath $(dirname "${SCRIPT_DIR}"))
 ROOT_IMAGE=registry.gitlab.com/batteriesincluded/batt-docker/batteries-debian12-build-tools:0.5.0
 USER_IMAGE=$(cor docker user-image ${ROOT_IMAGE})
 
-if [ -f "${GITHUB_ENV:-noSuchFile}" ]; then
-    ENV_FILE_FLAGS="--env-file ${GITHUB_ENV}"
-    cat "${GITHUB_ENV}"
-fi
-
 docker run \
        --ulimit memlock=-1:-1 \
        --cap-add SYS_ADMIN \
        --privileged \
        --network host \
-       ${ENV_FILE_FLAGS:-} \
+       --env CACHE_CONAN_REMOTE \
+       --env CACHE_CONAN_LOGIN_USERNAME \
+       --env CACHE_CONAN_PASSWORD \
        --volume "${HOME}/ci_conan_hosts:/etc/hosts:ro" \
        --volume "${PROJECT_DIR}:${PROJECT_DIR}" \
        --workdir "${PROJECT_DIR}" \
