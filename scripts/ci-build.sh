3e38941945064747f13d0763732f0a7850575e23
Anthony Astolfi <tastolfi@gmail.com>
2025-09-29 12:34:50 -0400

    Simplify ci-build.sh using cor select.

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -6,11 +6,15 @@ set -Eeuo pipefail
 #
 cor conan config install --type git https://gitlab.com/batteriesincluded/conan-config/linux-gcc12-x86_64.git
 
+# Select the build configuration.
+#
+cor select --clean --profile=linux-gcc12-x86_64 --build-type=Release
+
 # Build.
 #
 # cor install makes sure deps are uploaded to the cache server first;
 # TODO [tastolfi 2025-09-27] - Add a `cor pre-cache <options> <package_name>/<version> ...` command
 #
-cor install --clean --profile=linux-gcc12-x86_64 --build-type=Release
-cor build   --clean --profile=linux-gcc12-x86_64 --build-type=Release
-cor test    --only  --profile=linux-gcc12-x86_64 --build-type=Release
+cor install --clean
+cor build   --clean
+cor test    --only
