c6423b4d12c6a7695936cc64297770d6b33a1fb2
gbornste <gbornste@mathworks.com>
2026-03-27 13:44:16 -0400

    Optimize ChangeLogReader::visit_slots

--- src/turtle_kv/change_log/change_log_reader.hpp
+++ src/turtle_kv/change_log/change_log_reader.hpp
@@ -11,6 +11,7 @@
 
 #include <turtle_kv/change_log/change_log_block.hpp>
 #include <turtle_kv/change_log/edit_offset.hpp>
+#include <turtle_kv/util/stack_merger.hpp>
 
 #include <functional>
 
@@ -58,44 +59,94 @@ class ChangeLogReader
       return blocks.status();
     }
 
-    struct SlotEntry {
+    // Used to read slots from a block. Tracks which slot to read next with `next_slot_i`.
+    //
+    struct BlockIterator {
       boost::intrusive_ptr<ChangeLogBlock> block;
-      EditOffset edit_offset;
-      ConstBuffer payload;
+      usize next_slot_i = 0;
 
-      bool operator>(const SlotEntry& other) const
+      // Get the EditOffset of the current slot.
+      //
+      EditOffset current_edit_offset() const
       {
-        return this->edit_offset > other.edit_offset;
+        // TODO: [Gabe Bornstein 3/27/26] Is it too extreme to do a BATT_CHECK here?
+        //
+        BATT_CHECK(this->has_more());
+        ConstBuffer slot_buffer = this->block->get_slot(this->next_slot_i);
+        return EditOffset{*((little_i32*)slot_buffer.data())};
+      }
+
+      // Check if there are more slots to process.
+      //
+      bool has_more() const
+      {
+        return next_slot_i < block->slot_count();
+      }
+
+      bool operator<(const BlockIterator& other) const
+      {
+        // Both must have slots remaining for valid comparison.
+        //
+        // TODO: [Gabe Bornstein 3/27/26] Is it too extreme to do a BATT_CHECK here?
+        //
+        BATT_CHECK(this->has_more());
+        BATT_CHECK(other.has_more());
+        return this->current_edit_offset() < other.current_edit_offset();
       }
     };
 
-    constexpr usize kPerSlotEditOffsetDeltaOverhead = sizeof(little_i32);
+    struct BlockIteratorCompare {
+      bool operator()(BlockIterator* left, BlockIterator* right) const
+      {
+        return *left < *right;
+      }
+    };
 
-    // Put slots into a priority queue based on EditOffset.
+    // Create block iterators, filtering out empty blocks.
     //
-    std::priority_queue<SlotEntry, std::vector<SlotEntry>, std::greater<SlotEntry>> slot_queue;
-    for (const auto& block : *blocks) {
-      for (usize i = 0; i < block->slot_count(); ++i) {
-        ConstBuffer slot_buffer = block->get_slot(i);
-        EditOffset current_edit_offset = EditOffset{*((little_i32*)slot_buffer.data())};
+    std::vector<BlockIterator> block_iterators;
+    block_iterators.reserve(blocks->size());
 
-        slot_queue.push(SlotEntry{.block = block,
-                                  .edit_offset = current_edit_offset,
-                                  .payload = slot_buffer + kPerSlotEditOffsetDeltaOverhead});
+    for (auto& block : *blocks) {
+      if (block->slot_count() > 0) {
+        block_iterators.emplace_back(BlockIterator{
+            .block = std::move(block),
+            .next_slot_i = 0,
+        });
       }
     }
 
-    // Call visitor on the slot with the lowest EditOffset until all slots are processed.
+    // If there's no slots to process, return early.
     //
-    while (!slot_queue.empty()) {
-      const SlotEntry& current_slot = slot_queue.top();
+    if (block_iterators.empty()) {
+      LOG(INFO) << "No slots to be processed in change log.";
+      return batt::OkStatus();
+    }
 
-      Status visit_status =
-          visitor(current_slot.block.get(), current_slot.edit_offset, current_slot.payload);
+    StackMerger<BlockIterator, BlockIteratorCompare> heap{
+        Slice<BlockIterator>{as_slice(block_iterators)}};
 
+    constexpr usize kPerSlotEditOffsetDeltaOverhead = sizeof(little_i32);
+
+    // Process slots in EditOffset order.
+    //
+    while (!heap.empty()) {
+      BlockIterator* current = heap.first();
+
+      ConstBuffer slot_buffer = current->block->get_slot(current->next_slot_i);
+      EditOffset edit_offset = current->current_edit_offset();
+      ConstBuffer payload = slot_buffer + kPerSlotEditOffsetDeltaOverhead;
+
+      Status visit_status = visitor(current->block.get(), edit_offset, payload);
       BATT_REQUIRE_OK(visit_status);
 
-      slot_queue.pop();
+      current->next_slot_i++;
+
+      if (current->has_more()) {
+        heap.update_first();
+      } else {
+        heap.remove_first();
+      }
     }
 
     return batt::OkStatus();
